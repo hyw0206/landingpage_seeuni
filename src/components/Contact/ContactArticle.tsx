5ff843f26e3c767e2e1e33fd0bb827b3108409cd
noa <kim71665646@gmail.com>
2024-11-27 02:33:47 +0900

    Contact us Finish

--- src/components/Contact/ContactArticle.tsx
+++ src/components/Contact/ContactArticle.tsx
@@ -1,10 +1,8 @@
-import * as Styled from 'elements/Contact/ContactArticle';
-
 export default function ContactArticle() {
 
   return (
-    <Styled.ContactArticleWrapper>
-        <Styled.ContactArticleTitleText>To Contact us.</Styled.ContactArticleTitleText>
-    </Styled.ContactArticleWrapper>
+    <div>
+      Request
+    </div>
   )
 }
--- src/elements/Contact/Contact.ts
+++ src/elements/Contact/Contact.ts
@@ -5,5 +5,4 @@ export const ContactWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 458px; // height 추가
 `
