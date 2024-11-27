e2c578622ede0cbeba6362f95d920aace404cda7
noa <kim71665646@gmail.com>
2024-11-27 18:42:06 +0900

    Contact Request Finish

--- src/components/Contact/ContactRequest.tsx
+++ src/components/Contact/ContactRequest.tsx
@@ -6,28 +6,36 @@ export default function Request() {
         <Styled.ContactRequestWrapper>
         <Styled.ContactRequestTitleText>Please leave your request!</Styled.ContactRequestTitleText>
           <Styled.ContactInputWrapper>
-            <Styled.ContactRequestText>*Company name</Styled.ContactRequestText>
+            <Styled.ContactRequestTextWrapper>
+                <p>*</p><Styled.ContactRequestText>Company name</Styled.ContactRequestText>
+            </Styled.ContactRequestTextWrapper>
             <Styled.ContactRequestInput
               type="text"
               name="companyName"
               placeholder="Type your company name here"
             />
-  
-            <Styled.ContactRequestText>*E-mail</Styled.ContactRequestText>
+
+            <Styled.ContactRequestTextWrapper>
+                <p>*</p><Styled.ContactRequestText>E-mail</Styled.ContactRequestText>
+            </Styled.ContactRequestTextWrapper>
             <Styled.ContactRequestInput
               type="email"
               name="email"
               placeholder="Type email address here"
             />
-  
-            <Styled.ContactRequestText>Subject</Styled.ContactRequestText>
+
+            <Styled.ContactRequestTextWrapper>
+                <Styled.ContactRequestText>Subject</Styled.ContactRequestText>
+            </Styled.ContactRequestTextWrapper>
             <Styled.ContactRequestInput
               type="text"
               name="subject"
               placeholder="Type subject text here"
             />
-  
-            <Styled.ContactRequestText>Message</Styled.ContactRequestText>
+
+            <Styled.ContactRequestTextWrapper>
+                <Styled.ContactRequestText>Message</Styled.ContactRequestText>
+            </Styled.ContactRequestTextWrapper>
             <Styled.ContactRequestTextArea
               name="message"
               placeholder="Type text here"
